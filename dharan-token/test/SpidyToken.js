1ff109fd69949539a277bdabc8c424ff8417bbc8
Dharan-07 <dharanishdharan2135@gmail.com>
2026-05-06 13:47:56 +0530

    created simple test deployment and transaction

--- dharan-token/test/SpidyToken.js
+++ dharan-token/test/SpidyToken.js
@@ -1,12 +1,48 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+import { expect } from "chai";
+import hre from "hardhat";
+
+const { ethers } = hre;
+
 
 describe('SpidyToken',()=>{
     let SpidyToken, spidyToken, owner, addr1, addr2;
+    const decimal = 18n;
+    const unit = 10n ** decimal;
 
     beforeEach(async()=>{
         SpidyToken = await ethers.getContractFactory('SpidyToken');
-        spidyToken = await SpidyToken.deploy();
-        [owner,addr1,addr2,_] = await ethers.getSigners();
+        [owner,addr1,addr2,] = await ethers.getSigners();
+        spidyToken = await SpidyToken.deploy(1000);
     })
+
+    describe('deployment',()=>{
+        it('Should set the right owner',async()=>{
+            await expect(spidyToken.connect(addr1).burn()).to.be.revertedWith("only owner can call");
+            await expect(spidyToken.connect(owner).burn()).to.not.be.reverted;
+        }); 
+
+        it('should assign the initial supply of token to the owner',async()=>{
+            const ownerBalance = await spidyToken.balanceOf(owner.address);
+            const totalSupply = await spidyToken.totalSupply();
+            const expectedInitial = totalSupply / 10n; // initialSupply = totalSupply / 10
+            expect(ownerBalance).to.equal(expectedInitial);
+        });
+    });
+
+    describe('transaction',()=>{
+        it('should transfer tokens between accounts',async()=>{
+
+            await spidyToken.connect(owner).transfer(addr1.address,10n);
+            const addr1Balance = await spidyToken.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(10n * unit);
+
+            await spidyToken.connect(addr1).transfer(addr2.address,10n);
+            const addr2Balance = await spidyToken.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(10n * unit);
+        });
+
+        // it('should fail when trying to transfer more tokens than the balance',async()=>{
+        //     await expect(spidyToken.connect(addr1).transfer(addr2.address,100n)).to.be.revertedWith("Not enough balance");
+        // });
+    });
 });
